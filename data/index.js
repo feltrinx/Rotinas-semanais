ccab877bb3c241049f46d300466cb477236622d0
unknown <gabrielcfeltrin556@gmail.com>
2024-04-09 14:17:45 -0300

    json data

---
+++ data/index.js
@@ -0,0 +1,58 @@
+const DATA = [{
+
+    porcentagem = "25%",
+
+    texto = "dormir",
+    
+    titulo = "descansar",
+    
+    Image = "https://cdn.pixabay.com/photo/2017/07/07/04/58/images-2480422_1280.jpg"
+    
+},
+{
+
+    porcentagem = "45%",
+
+    texto = "comer",
+    
+    titulo = "almoço",
+    
+    Image = "https://cdn.pixabay.com/photo/2017/06/27/18/24/images-2448356_1280.jpg",
+    
+ 
+
+
+ },
+ {
+    porcentagem = "70%",
+    
+    texto = "academia",
+    
+    titulo = "treinar",
+    
+    Image = "https://cdn.pixabay.com/photo/2017/06/29/06/06/dog-2453179_640.jpg"
+    
+},
+{
+    porcentagem = "5%",
+    
+    texto = "escola",
+    
+    titulo = "meus amigos",
+    
+    Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIS7u4Mmz7Py6-eYLZjO22hQG752OtvWz8xciWBvFyOYoN511g7lzc3nZu-5iElmemiho&usqp=CAU"
+    },
+   
+   { 
+    porcentagem = "23%",
+    
+    texto = "paseeio",
+    
+    titulo = "me divertir",
+    
+    Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPYPzgOU_Qk9LCDwtdRIiL4lLD3541SzpGR5VrgCrtlPv-nT5HpqfP28KeNovls4mUcwQ&usqp=CAU"
+    },
+
+]
+
+export default DATA;
